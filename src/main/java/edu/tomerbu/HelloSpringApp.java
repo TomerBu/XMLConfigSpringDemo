a520303e7d0fe6947d261f6aaddf62b1e462a393
TomerBu <Tomer.bu@gmail.com>
2017-02-14 12:23:56 +0200

    Property Place Holder file

--- src/main/java/edu/tomerbu/HelloSpringApp.java
+++ src/main/java/edu/tomerbu/HelloSpringApp.java
@@ -16,6 +16,7 @@ public class HelloSpringApp {
         System.out.println(theCoach.getDailyWorkout());
         System.out.println(theCoach.getFortune());
         System.out.println(theCoach);
+
         //cleanup:
         context.close();
 
